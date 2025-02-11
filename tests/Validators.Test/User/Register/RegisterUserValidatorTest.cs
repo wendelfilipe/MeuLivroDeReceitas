c2e87641bd68242d7776df3875d66e78bb77a5d5
Wendel Silva <wendelfilipe1997@hotmail.com>
2025-02-10 22:03:23 -0300

    Creating unit test
    Started to create a unit test
    Created folders and project to organaze our project

--- tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,12 +1,20 @@
-using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyRecipeBook.Application.UseCases.User.Register;
 
-namespace Validators.Tests.User.Register
-
-public class RegisterUserValidatorTest
+namespace Validators.Test.User.Register
 {
-    [Fact]
-    public void Sucess()
+    public class RegisterUserValidatorTest
     {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
 
+            var result = validator.Validate(request);
+        }
     }
 }
